76a05c64d8d451588e768a74351baa9545d530ca
AnadiSharma007 <anadis764@gmail.com>
2022-08-08 11:46:13 +0530

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const CONTRACT_ADDRESS = '0xC69bCE07D2109609884b7AdAd49BEd3d45750bD8';
+
+
+const transformCharacterData = (characterData) => {
+    return {
+        name: characterData.name,
+        imageURI: characterData.imageURI,
+        hp: characterData.hp.toNumber(),
+        maxHp: characterData.maxHp.toNumber(),
+        attackDamage: characterData.attackDamage.toNumber(),
+    };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
